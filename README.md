c2053847dc7905663ef455e140793620c60b2375
Arne Brasseur <arne@arnebrasseur.net>
2021-09-17 16:44:56 +0200

    Extract from webstuff

---
+++ README.md
@@ -0,0 +1,101 @@
+# hiccup
+
+<!-- badges -->
+[![cljdoc badge](https://cljdoc.org/badge/com.lambdaisland/hiccup)](https://cljdoc.org/d/com.lambdaisland/hiccup) [![Clojars Project](https://img.shields.io/clojars/v/com.lambdaisland/hiccup.svg)](https://clojars.org/com.lambdaisland/hiccup)
+<!-- /badges -->
+
+Enlive-backed Hiccup implementation (clj-only)
+
+## Features
+
+- Auto-escape strings
+- Fragments (`[:<> ...]`)
+- Components (`[my-fn ...]`)
+- Style maps (`[:div {:style {:color "blue"}}]`)
+
+This makes it behave closer to how Hiccup works in Reagent, reducing cognitive
+overhead when doing cross-platform development.
+
+<!-- installation -->
+## Installation
+
+To use the latest release, add the following to your `deps.edn` ([Clojure CLI](https://clojure.org/guides/deps_and_cli))
+
+```
+com.lambdaisland/hiccup {:mvn/version "0.0.0"}
+```
+
+or add the following to your `project.clj` ([Leiningen](https://leiningen.org/))
+
+```
+[com.lambdaisland/hiccup "0.0.0"]
+```
+<!-- /installation -->
+
+## Usage
+
+```clojure
+(require '[lambdaisland.hiccup :as h])
+
+(h/render [:html [:body ...]])
+```
+
+<!-- opencollective -->
+## Lambda Island Open Source
+
+<img align="left" src="https://github.com/lambdaisland/open-source/raw/master/artwork/lighthouse_readme.png">
+
+&nbsp;
+
+hiccup is part of a growing collection of quality Clojure libraries created and maintained
+by the fine folks at [Gaiwan](https://gaiwan.co).
+
+Pay it forward by [becoming a backer on our Open Collective](http://opencollective.com/lambda-island),
+so that we may continue to enjoy a thriving Clojure ecosystem.
+
+You can find an overview of our projects at [lambdaisland/open-source](https://github.com/lambdaisland/open-source).
+
+&nbsp;
+
+&nbsp;
+<!-- /opencollective -->
+
+<!-- contributing -->
+## Contributing
+
+Everyone has a right to submit patches to hiccup, and thus become a contributor.
+
+Contributors MUST
+
+- adhere to the [LambdaIsland Clojure Style Guide](https://nextjournal.com/lambdaisland/clojure-style-guide)
+- write patches that solve a problem. Start by stating the problem, then supply a minimal solution. `*`
+- agree to license their contributions as MPL 2.0.
+- not break the contract with downstream consumers. `**`
+- not break the tests.
+
+Contributors SHOULD
+
+- update the CHANGELOG and README.
+- add tests for new functionality.
+
+If you submit a pull request that adheres to these rules, then it will almost
+certainly be merged immediately. However some things may require more
+consideration. If you add new dependencies, or significantly increase the API
+surface, then we need to decide if these changes are in line with the project's
+goals. In this case you can start by [writing a pitch](https://nextjournal.com/lambdaisland/pitch-template),
+and collecting feedback on it.
+
+`*` This goes for features too, a feature needs to solve a problem. State the problem it solves, then supply a minimal solution.
+
+`**` As long as this project has not seen a public release (i.e. is not on Clojars)
+we may still consider making breaking changes, if there is consensus that the
+changes are justified.
+<!-- /contributing -->
+
+<!-- license -->
+## License
+
+Copyright &copy; 2021 Arne Brasseur and Contributors
+
+Licensed under the term of the Mozilla Public License 2.0, see LICENSE.
+<!-- /license -->
---
+++ src/lambdaisland/hiccup.clj
@@ -0,0 +1,102 @@
+(ns lambdaisland.hiccup
+  "Convert Hiccup to clojure.xml style :tag/:attrs/:content maps, with support
+  for :<>, function components, and extensible via protocol.
+
+  Anywhere where we need to render Hiccup we should prefer to go through this
+  namespace."
+  (:require [net.cgrand.enlive-html :as enlive]
+            [garden.compiler :as gc]
+            [clojure.string :as str]))
+
+(def block-level-tag?
+  #{:head :body :meta :title :script :svg :iframe :style
+    :link :address :article :aside :blockquote :details
+    :dialog :dd :div :dl :dt :fieldset :figure :figcaption :footer :form
+    :h1 :h2 :h3 :h4 :h5 :h6 :header :hgroup :hr :li :main :nav :ol :p :pre :section :table :ul})
+
+(defn- attr-map? [node-spec]
+  (and (map? node-spec) (not (keyword? (:tag node-spec)))))
+
+(defn- nodify [node-spec {:keys [newlines?] :as opts}]
+  (cond
+    (string? node-spec) node-spec
+    (vector? node-spec)
+    (let [[tag & [m & ms :as more]] node-spec]
+      (cond
+        (= :<> tag)
+        (enlive/flatmap #(nodify % opts) more)
+
+        (keyword? tag)
+        (let [[tag-name & segments] (.split (name tag) "(?=[#.])")
+              id (some (fn [^String seg]
+                         (when (= \# (.charAt seg 0)) (subs seg 1))) segments)
+              classes (keep (fn [^String seg]
+                              (when (= \. (.charAt seg 0)) (subs seg 1)))
+                            segments)
+              node {:tag (keyword tag-name) :attrs (if (attr-map? m) m {})
+                    :content (enlive/flatmap #(nodify % opts) (if (attr-map? m) ms more))}
+              node (if id (assoc-in node [:attrs :id] id) node)
+              node (if (seq classes)
+                     (update-in node
+                                [:attrs :class]
+                                (fn [kls]
+                                  (concat classes (if (string? kls) [kls] kls))))
+                     node)]
+          (cond-> node
+            (map? (get-in node [:attrs :style]))
+            (update-in [:attrs :style] (fn [style]
+                                         (-> (gc/compile-css [:& style])
+                                             (str/replace #"^\s*\{|\}\s*$" "")
+                                             str/trim)))
+            (sequential? (get-in node [:attrs :class]))
+            (update-in [:attrs :class] #(str/join " " %))
+            (and newlines? (block-level-tag? tag))
+            (->> (list "\n"))))
+
+        (or (fn? tag) (= :lambdaisland.ornament/styled (type tag)))
+        (nodify (apply tag more) opts)
+
+        :else
+        (throw (ex-info "Not a valid hiccup tag" {:tag tag :form node-spec}))))
+    (sequential? node-spec) (enlive/flatmap #(nodify % opts) node-spec)
+    (map? node-spec) (update-in node-spec [:content] (comp #(nodify % opts) seq))
+    :else (str node-spec)))
+
+;; TODO: this isn't super clean since we want :newlines? to be opt-in, but we
+;; don't have a way to pass options here because it's varargs. At the same time
+;; we mainly use this with a single arg, and also the name isn't great,
+;; something less generic would be nice... so this API call could stand some
+;; rethinking
+
+(defn html
+  "Convert hiccup to clojure.xml.
+  Like net.cgrand.enlive/html, but additionally support function components,
+  fragments with :<>, and extensible via the HiccupTag protocol."
+  [& nodes-specs]
+  (enlive/flatmap #(nodify % {:newlines? true}) nodes-specs))
+
+;;https://www.w3.org/TR/html5/syntax.html#void-elements
+(def html5-void-elements
+  #{:area :base :br :col :embed :hr :img :input :keygen :link :meta :param
+    :source :track :wbr})
+
+(defn render-html*
+  "Render clojure.xml to string.
+  Emits void-tags for HTML5 void elements (no closing tag or self-closing)."
+  [h]
+  (with-redefs [enlive/self-closing-tags html5-void-elements]
+    (apply str (enlive/emit* h))))
+
+(defn render-html
+  "Render clojure.xml to string, add DOCTYPE"
+  [h]
+  (str "<!DOCTYPE html>\n" (render-html* h)))
+
+(defn render
+  "Render hiccup to string"
+  ([h]
+   (render h nil))
+  ([h {:keys [doctype?]
+       :or {doctype? true}
+       :as opts}]
+   ((if doctype? render-html render-html*) (nodify h opts))))
